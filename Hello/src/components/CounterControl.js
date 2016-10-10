81d9b3391b31ae0a251cd26b6eb503d475e068b6
nattyait <nattanicha@gmail.com>
2016-10-10 14:09:55 +0700

    use bindActionCreator and connect and get rid of context and action binding

--- Hello/src/components/CounterControl.js
+++ Hello/src/components/CounterControl.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
 import {
   increaseCounter,
   decreaseCounter,
@@ -7,49 +10,67 @@ import {
 } from '../actions/counterActions'
 
 class CounterControl extends React.Component{
-  constructor(props){
-    super(props)
-    this.onClickIncrease=this.onClickIncrease.bind(this)
-    this.onClickDecrease=this.onClickDecrease.bind(this)
-    this.onClickReset=this.onClickReset.bind(this)
-  }
-
-  onClickIncrease(){
-    //this.props.onValueChange(this.props.currentValue + 1)
-    //this.props.store.dispatch(increaseCounter())
-    this.context.store.dispatch(increaseCounter())
-  }
-  onClickDecrease(){
-    //this.props.onValueChange(this.props.currentValue - 1)
-    //this.props.store.dispatch(decreaseCounter())
-    this.context.store.dispatch(decreaseCounter())
-  }
-  onClickReset(){
-    //this.props.onValueChange(0)
-    //this.props.store.dispatch(resetCounter())
-    this.context.store.dispatch(resetCounter())
-  }
+  // constructor(props){
+  //   super(props)
+  //   this.onClickIncrease=this.onClickIncrease.bind(this)
+  //   this.onClickDecrease=this.onClickDecrease.bind(this)
+  //   this.onClickReset=this.onClickReset.bind(this)
+  // }
+  //
+  // onClickIncrease(){
+  //   //this.props.onValueChange(this.props.currentValue + 1)
+  //   //this.props.store.dispatch(increaseCounter())
+  //   //this.context.store.dispatch(increaseCounter())
+  //   this.props.store.dispatch(increaseCounter())
+  // }
+  // onClickDecrease(){
+  //   //this.props.onValueChange(this.props.currentValue - 1)
+  //   //this.props.store.dispatch(decreaseCounter())
+  //   //this.context.store.dispatch(decreaseCounter())
+  //   this.props.store.dispatch(decreaseCounter())
+  // }
+  // onClickReset(){
+  //   //this.props.onValueChange(0)
+  //   //this.props.store.dispatch(resetCounter())
+  //   //this.context.store.dispatch(resetCounter())
+  //   this.props.store.dispatch(resetCounter())
+  // }
 
   render(){
     return(
       <div>
-        <button onClick={this.onClickIncrease}>+</button>
-        <button onClick={this.onClickDecrease}>-</button>
-        <button onClick={this.onClickReset}>reset</button>
+        {/* <button onClick={this.onClickIncrease}>+</button> */}
+        <button onClick={this.props.increaseCounter}>+</button>
+        {/* <button onClick={this.onClickDecrease}>-</button> */}
+        <button onClick={this.props.decreaseCounter}>-</button>
+        {/* <button onClick={this.onClickReset}>reset</button> */}
+        <button onClick={this.props.resetCounter}>reset</button>
       </div>
     )
   }
 }
 
-//set อันนี้ไว้ จะทำให้ไม่ลืม parse prop เข้ามา
-//ควรเขียนตัวนี้ไว้เป็น document แล้วมันจะขึ้นใน console ด้วยถ้าไม่ส่งมา
-CounterControl.propTypes = {
-  currentValue: React.PropTypes.number.isRequired,
-  onValueChange: React.PropTypes.func.isRequired
+// //set อันนี้ไว้ จะทำให้ไม่ลืม parse prop เข้ามา
+// //ควรเขียนตัวนี้ไว้เป็น document แล้วมันจะขึ้นใน console ด้วยถ้าไม่ส่งมา
+// CounterControl.propTypes = {
+//   currentValue: React.PropTypes.number.isRequired,
+//   onValueChange: React.PropTypes.func.isRequired
+// }
+//
+// CounterControl.contextTypes = {
+//   store: React.PropTypes.object
+// }
+
+function mapDispatchToProps(dispatch) {
+  return {
+    // increaseCounter: () => {
+    //   dispatch(increaseCounter()) // key: function(dispatch(function))
+    // }
+    //after import bindActionCreators
+    increaseCounter: bindActionCreators(increaseCounter, dispatch),
+    decreaseCounter: bindActionCreators(decreaseCounter, dispatch),
+    resetCounter: bindActionCreators(resetCounter, dispatch)
+  }
 }
 
-CounterControl.contextTypes = {
-  store: React.PropTypes.object
-}
-
-export default CounterControl
+export default connect(null, mapDispatchToProps)(CounterControl)
