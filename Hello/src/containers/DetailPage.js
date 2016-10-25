a13686aae2a424265e99a445407b1e33ff749179
nattyait <nattanicha@gmail.com>
2016-10-25 17:14:04 +0700

    connect redux for detailPage

--- Hello/src/containers/DetailPage.js
+++ Hello/src/containers/DetailPage.js
@@ -1,13 +1,19 @@
 import React from 'react'
-
+import { connect } from 'react-redux'
 class DetailPage extends React.Component {
   render () {
     return (
       <div>
-        <h1>Detail Page</h1>
+        <h1>Detail Page {this.props.shot.title}</h1>
       </div>
     )
   }
 }
+//ownProps = ที่ข้างนอก parse มาให้มัน
+const mapStoreStateToProps = (state, ownProps) => {
+  return {
+    shot: state.shotById[ownProps.params.id]
+  }
+}
 
-export default DetailPage
+export default connect(mapStoreStateToProps)(DetailPage)
