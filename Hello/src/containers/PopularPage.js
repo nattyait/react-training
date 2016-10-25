124f2962dd520dbb05140e33256475d87a322d79
nattyait <nattanicha@gmail.com>
2016-10-25 15:50:41 +0700

    call dribbble api

--- Hello/src/containers/PopularPage.js
+++ Hello/src/containers/PopularPage.js
@@ -5,9 +5,22 @@ import shots from '../data/shots.json'
 //console.log(shots)
 
 class PopularPage extends React.Component {
+  state = {
+    shots: []
+  }
+  componentDidMount(){
+    fetch('https://api.dribbble.com/v1/shots?access_token=890076c326208858658efe077035f56ea2a2b478a438a621f639d6cdd660ea20')
+    .then((response) => {
+      return response.json()
+    }).then((json) => {
+      this.setState({
+        shots: json
+      })
+    })
+  }
   render () {
     return (
-      <ShortList shots={shots}/>
+      <ShortList shots={this.state.shots}/>
     )
   }
 }
